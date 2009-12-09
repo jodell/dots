70671ba312114df989aca9ac5d5e9e18a841d11f
Jeffrey ODell <jeffrey.odell@gmail.com>
2009-12-09 16:40:25 -0600

    cosmetic changes

--- linkify.rb
+++ linkify.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
-#require 'fileutils'
 
-dotsfiles = Dir['.*'].reject { |f| ['.', '..', '.git'].include?(f) } 
-#puts dotsfiles
-
-# Don't overwrite existing files for now
-dotsfiles.each do |f|
+Dir['.*'].each do |f| 
+  next if ['.', '..', '.git'].include?(f) 
+  # Don't deal with overwriting existing files for now 
   next if File.exists?(File.expand_path("~/#{File.basename(f)}"))
   `ln -s #{File.expand_path(f)} ~/#{File.basename(f)}`
 end
+  
