d2b33e6d98bb66c2dc783bcf3fdcb434c0d6fd38
Jeffrey ODell <jeffrey.odell@gmail.com>
2010-10-06 16:12:38 -0500

    move/rename files

--- bin/linkify.rb
+++ bin/linkify.rb
@@ -9,14 +9,14 @@ dirs = %w(
 
 dirs.each { |dir| `mkdir -p #{dir}` }
 pwd = File.expand_path(File.dirname(__FILE__))
-dotfiles_dir = "#{pwd}/../etc/dotsfiles"
+dotfiles_dir = "#{pwd}/../etc"
 
-Dir["#{dotfiles_dir}/.*"].each do |f| 
-  next if ['/.', '/..'].map { |s| dotfiles_dir + s }.include?(f) 
+Dir["#{dotfiles_dir}/*"].each do |f| 
+  next if ['/.', '/..'].map { |s| dotfiles_dir + s }.include?(f) || File.directory?(f)
   puts "Linking file #{f}"
   # Don't deal with overwriting existing files for now 
   #next if File.exists?(File.expand_path("~/#{File.basename(f)}"))
-  `ln -sf #{File.expand_path(f)} ~/#{File.basename(f)}`
+  `ln -sf #{File.expand_path(f)} ~/.#{File.basename(f)}`
 end
 
 pwd = File.dirname(File.expand_path(__FILE__))
